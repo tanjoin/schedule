624b8f75d11abc3644112739cfeaf4cae15b5276
tanjoin <1418838+tanjoin@users.noreply.github.com>
2018-01-28 15:16:40 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,30 @@
+window.onload = () => {
+  main();
+};
+
+const DayGrids = [
+  "grid-sun",
+  "grid-mon",
+  "grid-tue",
+  "grid-wed",
+  "grid-thu",
+  "grid-fri",
+  "grid-sat",
+  "grid-sun"
+];
+
+const main = () => {
+  setToday();
+};
+
+const setToday = () => {
+  var day = DayGrids[new Date().getDay()];
+  for (var i = 0; i < DayGrids.length; i++) {
+    const div = document.getElementById(DayGrid[i]);
+    if (DayGrids[i] === day) {
+      div.className += " today";
+    } else {
+      div.className += " other";
+    }
+  }
+};
